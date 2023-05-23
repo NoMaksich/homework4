d29323e507657a936fa7c11d6010ddf11ede28e8
maximmetalnikov <maxummetalnikov@mail.ru>
2023-05-23 12:50:40 +0300

    Выполнили задание три

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿void Function(int[] array)
+{
+    for(int i=0; i<8; i++)
+    {
+        array[i]=new Random().Next(0,100);
+    }
+    Console.Write("Значения массива: ");
+    for(int i=0; i<8; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+}
+
+int[] array=new int[8];
+Function(array);

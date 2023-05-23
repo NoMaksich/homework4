039f8bd2d2adc7d2eb099964f08ffa4cd502fa7e
maximmetalnikov <maxummetalnikov@mail.ru>
2023-05-23 12:50:33 +0300

    Выполнили задание два

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int Prompt()
+{
+    Console.Write("Введите число: ");
+    int limit =int.Parse(Console.ReadLine());
+    return limit;
+}
+int Sum(int number)
+{
+    int result=0;
+    while(number>0)
+    {
+        result+=number%10;
+        number=number/10;
+    }
+    return result;
+}
+int number=Prompt();
+int result=Sum(number);
+Console.WriteLine($"Сумма цифр числа {number} = {result}");
